47998e73dc7e9eb05e955aed71eec187ec6bdbc7
Tima02121991 <mirzakaev_timur@mail.ru>
2022-04-18 22:29:08 +0500

    write TestWeb.java

--- src/test/java/ru/netology/web/TestWeb.java
+++ src/test/java/ru/netology/web/TestWeb.java
@@ -1,10 +1,12 @@
 package ru.netology.web;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class TestWeb {
@@ -13,21 +15,21 @@ public class TestWeb {
 
 
     @BeforeAll
-    public static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
     public void setUp() {
-        driver  = new ChromeDriver();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
     }
+
     @Test
-    public void shouldSendForm(){
+    public void shouldSendForm() {
         driver.get("http://localhost:9999/");
         driver.findElement(By.name("name")).sendKeys("Петров Василий");
         driver.findElement(By.name("phone")).sendKeys("+79177774411");
@@ -35,10 +37,12 @@ public class TestWeb {
         driver.findElement(By.tagName("button")).click();
         String actualText = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText().trim();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        }
+        assertEquals(expected, actualText);
+    }
 
- /*   @AfterEach
-    public void tearDown(){
+    @AfterEach
+    public void tearDown() {
         driver.quit();
-        driver = null;*/
+        driver = null;
+    }
 }
